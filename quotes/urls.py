60dba3924213e1a52643526052526ab722684725
KhaiiPhamm <khaipduc@bu.edu>
2026-09-16 19:02:27 -0400

    add a path for web to fall on if only at khaipduc/quotes

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path(r'quote',views.quote, name="quote"),
-    path(r'show_all', views.show_all, name="show_all"),
-    path(r'about', views.about, name="about"),
+    path('', views.quote, name='quote'),           # cs-webapps.bu.edu/khaipduc/quotes/
+    path(r'quote/',views.quote, name="quote"),
+    path(r'show_all/', views.show_all, name="show_all"),
+    path(r'about/', views.about, name="about"),
 ]
